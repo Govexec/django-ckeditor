4b485300bdfdbd50549a8099231c3da4ec8a23c3
Llewellyn Hinkes-Jones <lhinkesjones@theatlantic.com>
2012-05-03 10:06:54 -0400

    Cleaning up, removing unused imports

--- ckeditor/urls.py
+++ ckeditor/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-import os
 
 urlpatterns = patterns(
     '',
     url(r'^upload/', 'ckeditor.views.upload', name='ckeditor_upload'),
     
     url(r'^browse/', 'ckeditor.views.browse', name='ckeditor_browse'),
-    
-#    url(r"^static/(?P<path>.*)$", "django.views.static.serve", {"document_root": os.path.dirname(__file__) + "/static/ckeditor/ckeditor"}, "ckeditor_static"),
 )
--- ckeditor/widgets.py
+++ ckeditor/widgets.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.utils.functional import lazy
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
@@ -15,70 +14,69 @@ from django.forms.util import flatatt
 json_encode = simplejson.JSONEncoder().encode
 
 DEFAULT_CONFIG = {
-    'skin': 'v2',
-    'toolbar': 'Full',
-    'height': 291,
-    'width': 618,
-    'filebrowserWindowWidth': 940,
-    'filebrowserWindowHeight': 747,
-    'filebrowserBrowsePatternName': 'ckeditor_browse',
+	'skin': 'v2',
+	'toolbar': 'Full',
+	'height': 291,
+	'width': 618,
+	'filebrowserWindowWidth': 940,
+	'filebrowserWindowHeight': 747,
+	'filebrowserBrowsePatternName': 'ckeditor_browse',
 	'filebrowserUploadPatternName': 'ckeditor_upload',
 	'filebrowserBrowseParams' : '',
 	'filebrowserUploadParams' : ''
 }
 
-reverse_lazy = lazy(reverse, str)
 
 class CKEditorWidget(forms.Textarea):
-    """
-    Widget providing CKEditor for Rich Text Editing.
-    Supports direct image uploads and embed.
-    """
-    class Media:
-       js = (
-                settings.STANDARD_ADMIN_MEDIA_PREFIX + "ckeditor/ckeditor.js" ,
-        )
-       
-    def __init__(self, config_name='default', *args, **kwargs):
-        super(CKEditorWidget, self).__init__(*args, **kwargs)
-        # Setup config from defaults.
-        self.config = DEFAULT_CONFIG
+	"""
+	Widget providing CKEditor for Rich Text Editing.
+	Supports direct image uploads and embed.
+	"""
+	class Media:
+		js = (
+				settings.STANDARD_ADMIN_MEDIA_PREFIX + "ckeditor/ckeditor.js" ,
+		)
+	   
+	def __init__(self, config_name='default', *args, **kwargs):
+		super(CKEditorWidget, self).__init__(*args, **kwargs)
+		# Setup config from defaults.
+		self.config = DEFAULT_CONFIG
 
-        # Try to get valid config from settings.
-        configs = getattr(settings, 'CKEDITOR_CONFIGS', None)
-        if configs != None:
-            if isinstance(configs, dict):
-                # Make sure the config_name exists.
-                if configs.has_key(config_name):
-                    config = configs[config_name]
-                    # Make sure the configuration is a dictionary.
-                    if not isinstance(config, dict):
-                        raise ImproperlyConfigured('CKEDITOR_CONFIGS["%s"] \
-                                setting must be a dictionary type.' % \
-                                config_name)
-                    # Override defaults with settings config.
-                    self.config.update(config)
-                else:
-                    raise ImproperlyConfigured("No configuration named '%s' \
-                            found in your CKEDITOR_CONFIGS setting." % \
-                            config_name)
-            else:
-                raise ImproperlyConfigured('CKEDITOR_CONFIGS setting must be a\
-                        dictionary type.')
+		# Try to get valid config from settings.
+		configs = getattr(settings, 'CKEDITOR_CONFIGS', None)
+		if configs != None:
+			if isinstance(configs, dict):
+				# Make sure the config_name exists.
+				if configs.has_key(config_name):
+					config = configs[config_name]
+					# Make sure the configuration is a dictionary.
+					if not isinstance(config, dict):
+						raise ImproperlyConfigured('CKEDITOR_CONFIGS["%s"] \
+								setting must be a dictionary type.' % \
+								config_name)
+					# Override defaults with settings config.
+					self.config.update(config)
+				else:
+					raise ImproperlyConfigured("No configuration named '%s' \
+							found in your CKEDITOR_CONFIGS setting." % \
+							config_name)
+			else:
+				raise ImproperlyConfigured('CKEDITOR_CONFIGS setting must be a\
+						dictionary type.')
 
-    def render(self, name, value, attrs={}):
-        if value is None:
-            value = ''
-        final_attrs = self.build_attrs(attrs, name=name)
-        
-        self.config['filebrowserUploadUrl'] = reverse(self.config['filebrowserUploadPatternName']) + self.config['filebrowserUploadParams']
-        
-        self.config['filebrowserBrowseUrl'] = reverse(self.config['filebrowserBrowsePatternName']) + self.config['filebrowserBrowseParams']
-        
-        return mark_safe(render_to_string('ckeditor/widget.html', {
-            'final_attrs': flatatt(final_attrs),
-            'value': conditional_escape(force_unicode(value)),
-            'id': final_attrs['id'],
-            'config': json_encode(self.config)
-            })
-        )
+	def render(self, name, value, attrs={}):
+		if value is None:
+			value = ''
+		final_attrs = self.build_attrs(attrs, name=name)
+		
+		self.config['filebrowserUploadUrl'] = reverse(self.config['filebrowserUploadPatternName']) + self.config['filebrowserUploadParams']
+		
+		self.config['filebrowserBrowseUrl'] = reverse(self.config['filebrowserBrowsePatternName']) + self.config['filebrowserBrowseParams']
+		
+		return mark_safe(render_to_string('ckeditor/widget.html', {
+			'final_attrs': flatatt(final_attrs),
+			'value': conditional_escape(force_unicode(value)),
+			'id': final_attrs['id'],
+			'config': json_encode(self.config)
+			})
+		)
